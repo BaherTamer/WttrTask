9cff8dfe2da51836b9ca6561d18bbdb59dac9568
Baher Tamer <baher.appledev@gmail.com>
2026-08-27 14:01:43 +0300

    feat: create endpoint protocol and build request

---
+++ WttrTask/Modules/NetworkModule/Sources/NetworkModule/Models/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  NetworkModule
+//
+//  Created by Baher Tamer on 27/08/2026.
+//
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
