202c92cd6614e3f158b61438a96e5c681e41dc21
Baher Tamer <baher.appledev@gmail.com>
2026-08-27 14:53:11 +0300

    feat: create weather loading view

---
+++ WttrTask/Features/WttrWeather/Sources/WttrWeather/Presentation/SubViews/WeatherLoadingView.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLoadingView.swift
+//  WttrWeather
+//
+//  Created by Baher Tamer on 27/08/2026.
+//
+
+import SwiftUI
+
+struct WeatherLoadingView: View {
+    // MARK: - Body
+    var body: some View {
+        ProgressView()
+    }
+}
