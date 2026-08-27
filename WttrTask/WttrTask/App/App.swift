603064300de6328472ebdd45cfeb2e9d1345691d
Baher Tamer <baher.appledev@gmail.com>
2026-08-27 13:46:11 +0300

    Feature: Setup Project (#1)
    * chore: setup xcode project
    
    * chore: add base domain configuration
    
    * feat: support swift 6

---
+++ WttrTask/WttrTask/App/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  WttrTask
+//
+//  Created by Baher Tamer on 27/08/2026.
+//
+
+import SwiftUI
+
+@main struct WttrApp: App {
+    // MARK: - Body
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
